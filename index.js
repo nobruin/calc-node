8c91d9e5d2b6e3e822a22297f8058a756aac28c4
bruno.marins <nobruin@gmail.com>
2021-07-02 03:15:34 -0300

    cria modulo para operações matematicas simples

---
+++ index.js
@@ -0,0 +1,6 @@
+const calc = require("./module/operations");
+
+const x = 10;
+const y = 1;
+
+console.log(calc.sum(x, y));
---
+++ module/operations.js
@@ -0,0 +1,40 @@
+
+_dealWithResult = function (number){
+    let result = parseInt(number);
+
+    if(isNaN(result)){
+        throw new Error("Invalid Operation");
+    }
+
+    return result;
+}
+
+sum = function (n1, n2){
+    let number = _dealWithResult(n1 + n2);
+    return number;
+}
+
+sub = function(n1, n2){
+    let number = _dealWithResult(n1 - n2);
+
+    return number;
+}
+
+multiplication = function(n1, n2){
+    let number = _dealWithResult(n1 * n2);
+
+    return number;
+}
+
+division = function(n1, n2){
+    let number = _dealWithResult(n1 / n2);
+
+    return number;
+}
+
+module.exports = {
+    sum:sum,
+    subtraction:sub,
+    multi:multiplication,
+    div:division
+}
